740446b906f872ff0cbbe51e9351f7dd71266eec
Fatejian <fatejian@qq.com>
2021-12-09 14:34:35 +0800

    react-demo init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = 
+{
+  entry: './app.js', // 入口文件
+  output: 
+  {
+    path: path.resolve(__dirname, 'dist'), // 定义输出目录
+    filename: 'my-first-webpack.bundle.js'  // 定义输出文件名称
+  }
+};
