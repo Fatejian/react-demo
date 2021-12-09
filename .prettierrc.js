da33150ed5215780eb27f0716581bdbfb07d34ae
Fatejian <fatejian@qq.com>
2021-12-09 16:24:19 +0800

    完善eslint和prettier自动化规则

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,32 @@
-// .prettierrc.js
 module.exports = {
-  singleQuote: true, // 平常模式使用单引号
-  trailingComma: 'es5', // 末尾属性添加,
-  tabWidth: 2, // tab 为2个空格长度
-  semi: false, // 不需要分号
-  printWidth: 120, // 单行长度
-}
+  // 最大长度80个字符
+  printWidth: 80,
+  // 行末分号
+  semi: true,
+  // 单引号
+  singleQuote: true,
+  // JSX双引号
+  jsxSingleQuote: false,
+  // 尽可能使用尾随逗号（包括函数参数）
+  trailingComma: 'all',
+  // 在对象文字中打印括号之间的空格。
+  bracketSpacing: true,
+  // > 标签放在最后一行的末尾，而不是单独放在下一行
+  jsxBracketSameLine: false,
+  // 箭头圆括号
+  arrowParens: 'avoid',
+  // 在文件顶部插入一个特殊的 @format 标记，指定文件格式需要被格式化。
+  insertPragma: false,
+  // 缩进
+  tabWidth: 2,
+  // 使用tab还是空格
+  useTabs: false,
+  // 行尾换行格式
+  endOfLine: 'auto',
+  HTMLWhitespaceSensitivity: 'ignore',
+  extends: [
+    'eslint:recommended',
+    //避免与 prettier 冲突
+    'plugin:prettier/recommended',
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './app.js', // 入口文件
   output: {
     path: path.resolve(__dirname, 'dist'), // 定义输出目录
     filename: 'my-first-webpack.bundle.js', // 定义输出文件名称
-  },
+  }
 }
